b6bd6c9892df33bfb047b0b18f1e481db54ebb5e
Adin Vashi <akvashi24@gmail.com>
2022-01-15 18:34:40 -0500

    change it again

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,7 +7,7 @@ const TWITTER_LINK = "https://twitter.com/akvashi24"
 
 export default function Home() {
 
-    const redirectUri = process.env.NODE_ENV === "development" ? "http://localhost:3000/tools" : process.env.NODE_ENV === "production" ? "https://catalogger.xyz/tools" : ""
+    const redirectUri = process.env.NODE_ENV === "development" ? "http://localhost:3000/tools" : process.env.NODE_ENV === "production" ? "https://www.catalogger.xyz/tools" : ""
 
     const allSpotifyScopes = 'user-read-playback-state user-modify-playback-state user-read-currently-playing user-read-email user-follow-modify user-follow-read user-library-modify user-library-read streaming app-remote-control user-read-playback-position user-top-read user-read-recently-played playlist-modify-private playlist-read-collaborative playlist-read-private playlist-modify-public'
 
