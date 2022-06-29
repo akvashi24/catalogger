12749b6fba953bc3212512c7709faee1aef2276c
Adin Vashi <akvashi24@gmail.com>
2022-06-29 10:17:15 -0400

    hover on playlists

--- src/components/playlists.jsx
+++ src/components/playlists.jsx
@@ -13,7 +13,7 @@ export default function Playlists(props) {
         <div className='flex flex-col w-full overflow-scroll h-3/4'>
             {
                 props.playlists.map(playlist => {
-                    return <button key={playlist.id} id={playlist.id} onClick={props.handlePlaylistClick} className="flex px-8 py-5 mb-3 font-semibold text-center text-white rounded-lg bg-neutral-700">
+                    return <button key={playlist.id} id={playlist.id} onClick={props.handlePlaylistClick} className="flex px-8 py-5 mb-3 font-semibold text-center text-white rounded-lg bg-neutral-700 hover:bg-green-600">
                         <span className="mx-auto">{playlist.name}</span>
                     </button>
                 })
