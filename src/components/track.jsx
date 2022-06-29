ba227399c86f30bb656ea210d8ef6cb7e8b2b745
Adin Vashi <akvashi24@gmail.com>
2022-06-29 11:06:09 -0400

    fix track display

--- src/components/track.jsx
+++ src/components/track.jsx
@@ -3,7 +3,7 @@ export const Track = (props) => {
         <div className="flex flex-col justify-center">
             <img src={props.albumArt} className="object-cover mx-auto mb-6 max-w-80 max-h-80"></img>
             <div className="flex flex-col justify-center text-left text-white">
-                <span className="mb-2 text-2xl font-bold">{props.songTitle}</span>
+                <span className="text-2xl font-bold ">{props.songTitle}</span>
                 <span className="text-gray-300 text-l">{props.albumTitle}</span>
                 <span className="text-gray-300 text-g">{props.artistNames.join(', ')}</span>
             </div>
