64aef5eef8f02d384ff40f2fffe6e99605f86b11
Adin Vashi <akvashi24@gmail.com>
2022-10-01 18:36:47 -0400

    Skip to next on bad song

--- src/components/Tools.jsx
+++ src/components/Tools.jsx
@@ -1,125 +1,164 @@
-import { useEffect, useState, useContext } from 'react'
-import { getCurrentlyPlaying, getAllPlaylists, deleteSongFromPlaylist, addSongToPlaylist, skipToNext } from '../services/spotify'
-import CurrentlyPlaying from './currentlyPlaying'
-import twitter from '../assets/twitter.svg'
-import close from '../assets/close.png'
-import Playlists from './playlists'
-import UserContext from '../services/context'
-import DeleteFromPlaylist from './deleteFromPlaylist'
-import toast from 'react-hot-toast'
+import { useEffect, useState, useContext } from "react";
+import {
+  getCurrentlyPlaying,
+  getAllPlaylists,
+  deleteSongFromPlaylist,
+  addSongToPlaylist,
+  skipToNext,
+} from "../services/spotify";
+import CurrentlyPlaying from "./currentlyPlaying";
+import twitter from "../assets/twitter.svg";
+import close from "../assets/close.png";
+import Playlists from "./playlists";
+import UserContext from "../services/context";
+import DeleteFromPlaylist from "./deleteFromPlaylist";
+import toast from "react-hot-toast";
 
-const TWITTER_HANDLE = "akvashi24"
-const TWITTER_LINK = "https://twitter.com/akvashi24"
+const TWITTER_HANDLE = "akvashi24";
+const TWITTER_LINK = "https://twitter.com/akvashi24";
 
 export default function Tools() {
-    const [currentlyPlaying, setCurrentlyPlaying] = useState(null)
-    const [playlists, setPlaylists] = useState([])
-    const [pinnedPlaylist, setPinnedPlaylist] = useState(null)
-    const user = useContext(UserContext)
-    useEffect(
-        () => {
-            const reallyHighLimit = 50
-            console.log('checking for user context', user)
-            if (user) {
-                const req = getAllPlaylists(user.id, reallyHighLimit)
-                req.then(
-                    result => setPlaylists(result?.data.items)
-                )
-                console.debug("Playlists:", playlists)
-            }
-        }, [user]
-    )
-    useEffect(
-        () => {
-            const interval = setInterval(() => {
-                const req = getCurrentlyPlaying()
-                req.then(
-                    result => setCurrentlyPlaying(result.data)
-                )
-            }, 2000)
-
-
-            return () => clearInterval(interval)
-        }, []
-    )
-
-    const handlePin = (mouseEvent) => {
-        const [playlistId, playlistSnapshotId] = mouseEvent.currentTarget.id.split('-')
-        const playlistName = mouseEvent.currentTarget.childNodes[0].innerHTML
-        if (pinnedPlaylist?.id === playlistId) {
-            setPinnedPlaylist(null)
-        }
-        else {
-            setPinnedPlaylist({ id: playlistId, name: playlistName, snapshotId: playlistSnapshotId })
-        }
+  const [currentlyPlaying, setCurrentlyPlaying] = useState(null);
+  const [playlists, setPlaylists] = useState([]);
+  const [pinnedPlaylist, setPinnedPlaylist] = useState(null);
+  const user = useContext(UserContext);
+  useEffect(() => {
+    const reallyHighLimit = 50;
+    console.log("checking for user context", user);
+    if (user) {
+      const req = getAllPlaylists(user.id, reallyHighLimit);
+      req.then((result) => setPlaylists(result?.data.items));
+      console.debug("Playlists:", playlists);
     }
+  }, [user]);
+  useEffect(() => {
+    const interval = setInterval(() => {
+      const req = getCurrentlyPlaying();
+      req.then((result) => setCurrentlyPlaying(result.data));
+    }, 2000);
 
+    return () => clearInterval(interval);
+  }, []);
 
-    const handlePlaylistClick = (mouseEvent) => {
-        const targetPlaylistId = mouseEvent.currentTarget.id
-        const targetPlaylistName = mouseEvent.currentTarget.textContent
-        if (currentlyPlaying) {
-            let addReq = addSongToPlaylist([currentlyPlaying.item.uri], targetPlaylistId)
-            let deleteReq = deleteSongFromPlaylist([{ uri: currentlyPlaying.item.uri }], pinnedPlaylist?.id, pinnedPlaylist?.snapshotId)
-            addReq.then(
-                () => toast.success(`Added to ${targetPlaylistName}!`, { icon: '🎵' })
-            )
-            deleteReq.then(
-                () => toast.success(`Deleted from ${pinnedPlaylist.name}!`, { icon: '🎵' })
-            )
-        }
+  const handlePin = (mouseEvent) => {
+    const [playlistId, playlistSnapshotId] =
+      mouseEvent.currentTarget.id.split("-");
+    const playlistName = mouseEvent.currentTarget.childNodes[0].innerHTML;
+    if (pinnedPlaylist?.id === playlistId) {
+      setPinnedPlaylist(null);
+    } else {
+      setPinnedPlaylist({
+        id: playlistId,
+        name: playlistName,
+        snapshotId: playlistSnapshotId,
+      });
     }
+  };
 
-    const handleDeleteAndSkip = () => {
-        if (currentlyPlaying) {
-            let deleteReq = deleteSongFromPlaylist([{ uri: currentlyPlaying.item.uri }], pinnedPlaylist?.id, pinnedPlaylist?.snapshotId)
-            skipToNext()
-            deleteReq.then(
-                () => toast.success(`Deleted from ${pinnedPlaylist.name}!`, { icon: '🎵' })
-            )
-        }
+  const handlePlaylistClick = (mouseEvent) => {
+    const targetPlaylistId = mouseEvent.currentTarget.id;
+    const targetPlaylistName = mouseEvent.currentTarget.textContent;
+    if (currentlyPlaying) {
+      let addReq = addSongToPlaylist(
+        [currentlyPlaying.item.uri],
+        targetPlaylistId
+      );
+      let deleteReq = deleteSongFromPlaylist(
+        [{ uri: currentlyPlaying.item.uri }],
+        pinnedPlaylist?.id,
+        pinnedPlaylist?.snapshotId
+      );
+      addReq.then(() =>
+        toast.success(`Added to ${targetPlaylistName}!`, { icon: "🎵" })
+      );
+      deleteReq.then(() =>
+        toast.success(`Deleted from ${pinnedPlaylist.name}!`, { icon: "🎵" })
+      );
     }
+  };
 
+  const handleDeleteAndSkip = () => {
+    if (currentlyPlaying) {
+      let deleteReq = deleteSongFromPlaylist(
+        [{ uri: currentlyPlaying.item.uri }],
+        pinnedPlaylist?.id,
+        pinnedPlaylist?.snapshotId
+      );
+      skipToNext();
+      deleteReq.then(() =>
+        toast.success(`Deleted from ${pinnedPlaylist.name}!`, { icon: "🎵" })
+      );
+    }
+  };
 
-    return (
-        <div>
-            <div className="h-screen px-12 overflow-scroll text-center md:px-20 bg-zinc-900">
-                <div className="flex flex-col justify-between h-auto sm:h-full sm:flex-row">
-                    <div id='left-panel' className="flex flex-col hidden px-12 mt-48 w-80 sm:block">
-                        <DeleteFromPlaylist currentSong={currentlyPlaying?.item} playlists={playlists} pinned={pinnedPlaylist} handlePin={handlePin} />
-                    </div>
-                    <div id="center-panel" className="flex flex-col h-full max-w-80">
-                        <div className="pt-10 text-center">
-                            <div className="flex flex-col mb-8 sm:mb-24">
-                                <span className="mb-4 text-5xl font-bold tracking-tight text-white">Catalogger</span>
-                            </div>
-                            <div className="flex flex-col content-center w-full mx-auto overflow-wrap">
-                                <CurrentlyPlaying currentlyPlaying={currentlyPlaying} />
-                                {currentlyPlaying ?
-                                    <img alt="Delete and skip" className="w-5 h-5 mt-4 ml-6 cursor-pointer" src={close} onClick={handleDeleteAndSkip} />
-                                    : null
-                                }
-                            </div>
-                        </div>
-                        <div className="flex-grow"></div>
-                        <div className="items-center justify-center hidden pt-4 mb-4 sm:flex justify-self-end">
-                            <img alt="Twitter Logo" className="h-7 w-7" src={twitter} />
-                            <a
-                                className="font-semibold text-zinc-100 decoration-cyan-400"
-                                href={TWITTER_LINK}
-                                target="_blank"
-                                rel="noreferrer"
-                            >{`built by @${TWITTER_HANDLE}`}</a>
-                        </div>
-                    </div>
-                    <div id='mobile-view-panel' className="flex flex-col px-12 mt-8 sm:hidden sm:mt-48 max-w-80">
-                        <DeleteFromPlaylist currentSong={currentlyPlaying?.item} playlists={playlists} pinned={pinnedPlaylist} handlePin={handlePin} />
-                    </div>
-                    <div id='right-panel' className="flex flex-col h-64 px-12 mt-8 sm:mt-48 sm:w-80 sm:h-auto">
-                        <Playlists currentSong={currentlyPlaying?.item} playlists={playlists} handlePlaylistClick={handlePlaylistClick} />
-                    </div>
-                </div>
-            </div >
-        </div >
-    )
+  return (
+    <div>
+      <div className="h-screen px-12 overflow-scroll text-center md:px-20 bg-zinc-900">
+        <div className="flex flex-col justify-between h-auto sm:h-full sm:flex-row">
+          <div
+            id="left-panel"
+            className="flex flex-col hidden px-12 mt-48 w-80 sm:block"
+          >
+            <DeleteFromPlaylist
+              currentSong={currentlyPlaying?.item}
+              playlists={playlists}
+              pinned={pinnedPlaylist}
+              handlePin={handlePin}
+            />
+          </div>
+          <div id="center-panel" className="flex flex-col h-full max-w-80">
+            <div className="pt-10 text-center">
+              <div className="flex flex-col mb-8 sm:mb-24">
+                <span className="mb-4 text-5xl font-bold tracking-tight text-white">
+                  Catalogger
+                </span>
+              </div>
+              <div className="flex flex-col content-center w-full mx-auto overflow-wrap">
+                <CurrentlyPlaying currentlyPlaying={currentlyPlaying} />
+                {currentlyPlaying ? (
+                  <img
+                    alt="Delete and skip"
+                    className="w-5 h-5 mt-4 cursor-pointer"
+                    src={close}
+                    onClick={handleDeleteAndSkip}
+                  />
+                ) : null}
+              </div>
+            </div>
+            <div className="flex-grow"></div>
+            <div className="items-center justify-center hidden pt-4 mb-4 sm:flex justify-self-end">
+              <img alt="Twitter Logo" className="h-7 w-7" src={twitter} />
+              <a
+                className="font-semibold text-zinc-100 decoration-cyan-400"
+                href={TWITTER_LINK}
+                target="_blank"
+                rel="noreferrer"
+              >{`built by @${TWITTER_HANDLE}`}</a>
+            </div>
+          </div>
+          <div
+            id="mobile-view-panel"
+            className="flex flex-col px-12 mt-8 sm:hidden sm:mt-48 max-w-80"
+          >
+            <DeleteFromPlaylist
+              currentSong={currentlyPlaying?.item}
+              playlists={playlists}
+              pinned={pinnedPlaylist}
+              handlePin={handlePin}
+            />
+          </div>
+          <div
+            id="right-panel"
+            className="flex flex-col h-64 px-12 mt-8 sm:mt-48 sm:w-80 sm:h-auto"
+          >
+            <Playlists
+              currentSong={currentlyPlaying?.item}
+              playlists={playlists}
+              handlePlaylistClick={handlePlaylistClick}
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
--- src/services/spotify.js
+++ src/services/spotify.js
@@ -1,101 +1,103 @@
-
 import axios from "axios";
-import { checkRefresh, logErrors } from "./api"
+import { checkRefresh, logErrors } from "./api";
 
-const spotifyURL = "https://api.spotify.com/v1/"
+const spotifyURL = "https://api.spotify.com/v1/";
 
 const spotify = axios.create({
-    baseURL: spotifyURL
-})
+  baseURL: spotifyURL,
+});
 
-spotify.defaults.headers.common['Content-Type'] = 'application/json';
-
+spotify.defaults.headers.common["Content-Type"] = "application/json";
 
 const getConfig = () => {
-    checkRefresh()
-    const accessToken = window.localStorage.getItem('accessToken')
-    const config = { headers: { 'Authorization': `Bearer ${accessToken}` } }
-    return config
-}
+  checkRefresh();
+  const accessToken = window.localStorage.getItem("accessToken");
+  const config = { headers: { Authorization: `Bearer ${accessToken}` } };
+  return config;
+};
 
 export const getCurrentlyPlaying = async (code) => {
-    const config = getConfig()
-    const path = spotifyURL + 'me/player/currently-playing'
-    const result = await spotify.get(path, config).catch(error => {
-        // TODO: (akv) this doesn't work if the endpoint doesn't exist
-        logErrors(error);
-        return false;
-    })
-    return result
-}
+  const config = getConfig();
+  const path = spotifyURL + "me/player/currently-playing";
+  const result = await spotify.get(path, config).catch((error) => {
+    // TODO: (akv) this doesn't work if the endpoint doesn't exist
+    logErrors(error);
+    return false;
+  });
+  return result;
+};
 
 export const skipToNext = async () => {
-    const config = await getConfig()
-    const path = spotifyURL + 'me/player/next'
-    const result = await spotify.put(path, config).catch(error => {
-        // TODO: (akv) this doesn't work if the endpoint doesn't exist
-        logErrors(error);
-        return false;
-    })
-    return result
-}
+  const config = getConfig();
+  const path = spotifyURL + "me/player/next";
+  const result = await spotify.post(path, {}, config).catch((error) => {
+    // TODO: (akv) this doesn't work if the endpoint doesn't exist
+    logErrors(error);
+    return false;
+  });
+  return result;
+};
 
 export const getAllPlaylists = async (userId, limit) => {
-    const config = getConfig()
-    const path = spotifyURL + `users/${userId}/playlists?limit=${limit}`
-    const result = await spotify.get(path, config).catch(error => {
-        // TODO: (akv) this doesn't work if the endpoint doesn't exist
-        logErrors(error);
-        return false;
-    })
-    return result
-}
+  const config = getConfig();
+  const path = spotifyURL + `users/${userId}/playlists?limit=${limit}`;
+  const result = await spotify.get(path, config).catch((error) => {
+    // TODO: (akv) this doesn't work if the endpoint doesn't exist
+    logErrors(error);
+    return false;
+  });
+  return result;
+};
 
 export const addSongToPlaylist = async (songIds, playlistId) => {
-    const config = getConfig()
-    const path = spotifyURL + `playlists/${playlistId}/tracks`
-    const result = await spotify.post(path, songIds, config).catch(error => {
-        // TODO: (akv) this doesn't work if the endpoint doesn't exist
-        logErrors(error);
-        return false;
-    })
-    return result
-}
+  const config = getConfig();
+  const path = spotifyURL + `playlists/${playlistId}/tracks`;
+  const result = await spotify.post(path, songIds, config).catch((error) => {
+    // TODO: (akv) this doesn't work if the endpoint doesn't exist
+    logErrors(error);
+    return false;
+  });
+  return result;
+};
 
-export const deleteSongFromPlaylist = async (songIds, playlistId, snapshotId) => {
-    const config = getConfig()
-    const path = spotifyURL + `playlists/${playlistId}/tracks`
-    config.data = { tracks: songIds, snapshot_id: snapshotId }
-    const result = await spotify.delete(path, config).catch(error => {
-        // TODO: (akv) this doesn't work if the endpoint doesn't exist
-        logErrors(error);
-        return false;
-    })
-    return result
-}
+export const deleteSongFromPlaylist = async (
+  songIds,
+  playlistId,
+  snapshotId
+) => {
+  const config = getConfig();
+  const path = spotifyURL + `playlists/${playlistId}/tracks`;
+  config.data = { tracks: songIds, snapshot_id: snapshotId };
+  const result = await spotify.delete(path, config).catch((error) => {
+    // TODO: (akv) this doesn't work if the endpoint doesn't exist
+    logErrors(error);
+    return false;
+  });
+  return result;
+};
 
 export const getUserDetails = async () => {
-    const config = getConfig()
-    const path = spotifyURL + 'me'
-    const result = await spotify.get(path, config).catch(error => {
-        // TODO: (akv) this doesn't work if the endpoint doesn't exist
-        logErrors(error);
-        return false;
-    })
-    return result?.data
-}
-
+  const config = getConfig();
+  const path = spotifyURL + "me";
+  const result = await spotify.get(path, config).catch((error) => {
+    // TODO: (akv) this doesn't work if the endpoint doesn't exist
+    logErrors(error);
+    return false;
+  });
+  return result?.data;
+};
 
 export const playHeat = async () => {
-    const config = getConfig()
-    const path = spotifyURL + 'me/player/play'
-    const payload = { "context_uri": 'spotify:user:7832d6291d614118:playlist:4TvnzPNK0JQ1mOQyeLQgAG?si=8b02185844d04386' }
-    config.data = payload
-    console.log(config)
-    const result = spotify.put(path, config.data, config).catch(error => {
-        // TODO: (akv) this doesn't work if the endpoint doesn't exist
-        logErrors(error);
-        return false;
-    })
-    return result
-}
+  const config = getConfig();
+  const path = spotifyURL + "me/player/play";
+  const payload = {
+    context_uri:
+      "spotify:user:7832d6291d614118:playlist:4TvnzPNK0JQ1mOQyeLQgAG?si=8b02185844d04386",
+  };
+  const result = spotify.put(path, payload, config).catch((error) => {
+    // TODO: (akv) this doesn't work if the endpoint doesn't exist
+    logErrors(error);
+    return false;
+  });
+  return result;
+};
