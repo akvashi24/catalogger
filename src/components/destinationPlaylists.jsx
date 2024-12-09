cf70b95aa0c5296cfb417f4e5556feb8f0b948e0
Adin <akvashi24@gmail.com>
2024-12-09 14:06:40 -0500

    Add logging

--- src/components/destinationPlaylists.jsx
+++ src/components/destinationPlaylists.jsx
@@ -9,6 +9,7 @@ export default function DestinationPlaylists(props) {
             <div className='flex flex-col w-full overflow-scroll h-3/4'>
                 {
                     props.playlists.map(playlist => {
+                        console.debug('Destination Playlist', playlist)
                         return <button key={playlist.id} id={playlist.id} onClick={props.handlePlaylistClick} className="flex px-8 py-5 mb-3 font-semibold text-center text-white rounded-lg bg-neutral-700 border-neutral-700 border-2 hover:border-green-600">
                             <span className="mx-auto">{playlist.name}</span>
                         </button>
